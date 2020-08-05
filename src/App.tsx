3582b614b25f41d939156d47dcb09364d9585ccd
Tom Schindl <tom.schindl@bestsolution.at>
2020-08-05 12:56:49 +0200

    reproducer for relative-height problems in listbox

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,121 @@ import {Provider, defaultTheme, Button, Flex, ListBox, Item, Text} from '@adobe/
 
 function App() {
   return (
-    <div>Samples/Reproducers are in different branches</div>
+    <Provider theme={defaultTheme}>
+      <div style={{ display: "flex", height: "300px", backgroundColor: "#eeeeee", borderStyle: "solid", borderWidth: "2px", borderColor: "black", padding: "10px", overflow: "hidden"}}>
+        <Flex maxHeight="300px">
+          <ListBox minWidth="static-size-400">
+            <Item>
+              <Text>A</Text>
+            </Item>
+            <Item>
+              <Text>B</Text>
+            </Item>
+            <Item>
+              <Text>B</Text>
+            </Item>
+            <Item>
+              <Text>C</Text>
+            </Item>
+            <Item>
+              <Text>E</Text>
+            </Item>
+            <Item>
+              <Text>F</Text>
+            </Item>
+            <Item>
+              <Text>G</Text>
+            </Item>
+            <Item>
+              <Text>H</Text>
+            </Item>
+            <Item>
+              <Text>I</Text>
+            </Item>
+            <Item>
+              <Text>J</Text>
+            </Item>
+            <Item>
+              <Text>K</Text>
+            </Item>
+          </ListBox>
+        </Flex>
+        <Flex maxHeight="100%">
+          <ListBox minWidth="static-size-400">
+            <Item>
+              <Text>A</Text>
+            </Item>
+            <Item>
+              <Text>B</Text>
+            </Item>
+            <Item>
+              <Text>B</Text>
+            </Item>
+            <Item>
+              <Text>C</Text>
+            </Item>
+            <Item>
+              <Text>E</Text>
+            </Item>
+            <Item>
+              <Text>F</Text>
+            </Item>
+            <Item>
+              <Text>G</Text>
+            </Item>
+            <Item>
+              <Text>H</Text>
+            </Item>
+            <Item>
+              <Text>I</Text>
+            </Item>
+            <Item>
+              <Text>J</Text>
+            </Item>
+            <Item>
+              <Text>K</Text>
+            </Item>
+          </ListBox>
+        </Flex>
+        <Flex height="100%">
+          <ListBox minWidth="static-size-400">
+            <Item>
+              <Text>A</Text>
+            </Item>
+            <Item>
+              <Text>B</Text>
+            </Item>
+            <Item>
+              <Text>B</Text>
+            </Item>
+            <Item>
+              <Text>C</Text>
+            </Item>
+            <Item>
+              <Text>E</Text>
+            </Item>
+            <Item>
+              <Text>F</Text>
+            </Item>
+            <Item>
+              <Text>G</Text>
+            </Item>
+            <Item>
+              <Text>H</Text>
+            </Item>
+            <Item>
+              <Text>I</Text>
+            </Item>
+            <Item>
+              <Text>J</Text>
+            </Item>
+            <Item>
+              <Text>K</Text>
+            </Item>
+          </ListBox>
+        </Flex>
+      </div>
+    </Provider>
   );
 }
 
